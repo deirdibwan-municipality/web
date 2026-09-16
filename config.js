8976d059eb1d0a4a26dc6b6bd5cf756ee630f5cc
deirdibwan-municipality <muayad4200@gmail.com>
2026-09-16 17:34:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxtA7PmALAE6M-2WTuQfAloxJ-6hZJHJAp_22uhrrS8A6gPEna_r1s8JxYvNq9fLVP78w/exec
-  ";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxtA7PmALAE6M-2WTuQfAloxJ-6hZJHJAp_22uhrrS8A6gPEna_r1s8JxYvNq9fLVP78w/exec";
